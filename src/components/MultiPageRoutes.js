74a7c7c1810fd0171840b0150df43b7422e801b3
Igor <igorzinar@gmail.com>
2024-08-06 18:35:51 +0300

    update

--- src/components/MultiPageRoutes.js
+++ src/components/MultiPageRoutes.js
@@ -1,6 +1,6 @@
 import Home from "./home/Home";
 import About from "./about/About";
-import Portfolio from "./portfolio/Portfolio";
+// import Portfolio from "./portfolio/Portfolio";
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
