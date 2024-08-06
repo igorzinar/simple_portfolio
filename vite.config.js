fc9db55cc0c1d8ad33c6372de46e007a4ca621c6
Igor <igorzinar@gmail.com>
2024-08-06 20:29:41 +0300

    small fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    css: {
+        modules: {
+            localsConvention: 'camelCase',
+            generateScopedName: '[name]__[local]___[hash:base64:5]'
+        },
+    },
     resolve: {
         alias: {
             '@': '/src',
